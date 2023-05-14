518e4eee53dcb9be788b44b8923699133b38eaa7
kdeatley <kevdeatley@gmail.com>
2023-05-14 14:28:43 -0700

    about viewable without logging in, added howto

--- src/main/java/com/bebopt/app/views/howto/howtoview.java
+++ src/main/java/com/bebopt/app/views/howto/howtoview.java
@@ -1,4 +1,4 @@
-package com.bebopt.app.views.about;
+package com.bebopt.app.views.howto;
 
 import com.bebopt.app.views.MainLayout;
 import com.vaadin.flow.component.html.H2;
@@ -9,23 +9,24 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.router.Route;
 
-@PageTitle("About")
-@Route(value = "about", layout = MainLayout.class)
+
+@PageTitle("User-Guide")
+@Route(value = "user-guide", layout = MainLayout.class)
 @AnonymousAllowed
-public class AboutView extends VerticalLayout {
-
-    public AboutView() {
+public class howtoview extends VerticalLayout {
+    public howtoview() {
         setSpacing(false);
 
-        //Image img = new Image("images/empty-plant.png", "placeholder plant");
-        //img.setWidth("200px");
-        //add(img);
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
 
-        H2 header = new H2("About our team");
+        H2 header = new H2("Coming Soon");
         header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
         add(header);
-        add(new Paragraph("Sandmen is a team composed of members Kevin Deatley, Fariah Saleh, and John Vieyra. We are a group of students studying computer science at CSUSM. The product described in this document is the project we will be completing for our software engineering course."));
+        add(new Paragraph("A guide on how to use"));
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);

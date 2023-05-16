6344307bf56d7fb790e2d75aa4690b78df0646c4
Fariah Saleh <fariahsaleh@gmail.com>
2023-05-15 19:50:57 -0700

    merge

--- src/main/java/com/bebopt/app/views/about/AboutView.java
+++ src/main/java/com/bebopt/app/views/about/AboutView.java
@@ -2,30 +2,29 @@ package com.bebopt.app.views.about;
 
 import com.bebopt.app.views.MainLayout;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
-import jakarta.annotation.security.PermitAll;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 
 @PageTitle("About")
 @Route(value = "about", layout = MainLayout.class)
-@PermitAll
+@AnonymousAllowed
 public class AboutView extends VerticalLayout {
 
     public AboutView() {
         setSpacing(false);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+        //Image img = new Image("images/empty-plant.png", "placeholder plant");
+        //img.setWidth("200px");
+        //add(img);
 
         H2 header = new H2("About our team");
         header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
         add(header);
-        add(new Paragraph(""));
+        add(new Paragraph("Sandmen is a team composed of members Kevin Deatley, Fariah Saleh, and John Vieyra. We are a group of students studying computer science at CSUSM. The product described in this document is the project we will be completing for our software engineering course."));
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);

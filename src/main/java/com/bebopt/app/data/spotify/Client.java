5d3c4448168e6dc38630044193830704a17e2a13
Fariah Saleh <fariahsaleh@gmail.com>
2023-07-14 11:25:51 -0700

    update client secret

--- src/main/java/com/bebopt/app/data/spotify/Client.java
+++ src/main/java/com/bebopt/app/data/spotify/Client.java
@@ -2,7 +2,7 @@ package com.bebopt.app.data.spotify;
 
 public class Client {
     private static String clientID = "4d988bad11c14e7c844bd1b166b8a8ad";
-    private static String clientSecret = "234b0b32bd13415fbe90b169c8c07cab";
+    private static String clientSecret = "8f4b649440a14969996d221c06e25007";
 
     public static String getClientID() {
         return clientID;
